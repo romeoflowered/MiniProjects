a10db256d779dc21a9d26755e3c77e44dc69c509
romeoflowered <ya.romantipyt@yandex.by>
2022-06-28 19:09:22 +0300

    Day_5

--- Day_5/Day_5.py
+++ Day_5/Day_5.py
@@ -1,7 +1,10 @@
-#Password Generator Project
+# Password Generator Project
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-           'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z',
+           'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+           'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -35,13 +38,13 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 password_list = []
 
 for char in range(1, nr_letters + 1):
-  password_list.append(random.choice(letters))
+    password_list.append(random.choice(letters))
 
 for char in range(1, nr_symbols + 1):
-  password_list += random.choice(symbols)
+    password_list += random.choice(symbols)
 
 for char in range(1, nr_numbers + 1):
-  password_list += random.choice(numbers)
+    password_list += random.choice(numbers)
 
 print(password_list)
 random.shuffle(password_list)
@@ -49,7 +52,7 @@ print(password_list)
 
 password = ""
 for char in password_list:
-  password += char
+    password += char
 
 print(f"Your password is: {password}")
 print(password)
