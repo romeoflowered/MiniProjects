53e1a3c1704f663da53f5c4e24a1288263448356
romeoflowered <ya.romantipyt@yandex.by>
2022-07-02 00:01:42 +0300

    Reeborg's World Maze

---
+++ Day_6/Day_6.py
@@ -0,0 +1,33 @@
+#Reeborg's World Maze
+# def move_1():
+#     while wall_in_front():
+#         turn_left()
+#     else:
+#         move()
+#     while not wall_on_right():
+#         turn_right()
+#         move()
+#
+#
+# while not at_goal():
+#     move_1()
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if front_is_clear():
+        move()
+    elif right_is_clear():
+        turn_right()
+        move()
+    else:
+        turn_left()
