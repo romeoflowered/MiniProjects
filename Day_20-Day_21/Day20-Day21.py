8dcf837ef5dab9e59d7a69b482952365a0cdfee6
romeoflowered <ya.romantipyt@yandex.by>
2022-08-19 20:03:58 +0300

    Snake Game

--- Day_20-Day_21/Day20-Day21.py
+++ Day_20-Day_21/Day20-Day21.py
@@ -27,7 +27,7 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-    #Detect collision with food.
+    # Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
