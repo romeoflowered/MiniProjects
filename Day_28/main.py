783ab431f0b28ead5dbd18cd7aad1800c6cfe029
romeoflowered <ya.romantipyt@yandex.by>
2022-09-12 15:04:55 +0300

    Pomodoro

--- Day_28/main.py
+++ Day_28/main.py
@@ -12,8 +12,8 @@ LONG_BREAK_MIN = 20
 reps = 0
 timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- #
 
+# ---------------------------- TIMER RESET ------------------------------- #
 def reset_timer():
     window.after_cancel(timer)
     canvas.itemconfig(timer_text, text="00:00")
@@ -80,18 +80,13 @@ canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer, highlightbackground=YELLOW)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer, highlightbackground=YELLOW)
 reset_button.grid(column=2, row=2)
 
 check_marks = Label(fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
-
-
-
-
-
 
 window.mainloop()
